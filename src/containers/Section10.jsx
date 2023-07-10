1cef62f19c7d3bf468f2b85cfc5ef8ae4d713ec1
Sarthak <notsarthakshah@gmail.com>
2023-07-11 00:04:37 +0530

    empty

--- src/containers/Section10.jsx
+++ src/containers/Section10.jsx
@@ -3,30 +3,39 @@ import '../App.css'
 
 const Section10 = () => {
   return (
+    <div className='section10'>
     <section className='newPost'>
       <div className='container boxItems'>  
+      <h1>Add Blog!</h1>
         <form>
           <div className='inputfile flexCenter'>
             <label>Upload Image</label>
             <input type='file' accept='image/*' alt='img' />
           </div>
+          <br/>
             <label>Title</label>
             <input type='text' placeholder='Title' />
+            <br/>
             <label>Description</label>
             <textarea name='description' id='' cols='30' rows='2' placeholder='Description'></textarea>
+            <br/>
             <label>Author's Name</label>
             <input type='text' placeholder='Name' />
+            <br/>
             <label>Blog</label>
-            <textarea name='blog' id='' cols='30' rows='10' placeholder='Blog'></textarea>
+            <textarea name='blog' id='' placeholder='Blog'></textarea>
+            <br/>
             <button className='button'>Create Post</button>
             <select id="mounth">
               <option value="hide">-- Type --</option>
               <option value="public">Public</option>
               <option value="private">Private</option>
             </select>
+            <br/>
         </form>
       </div>
     </section>
+    </div>
   )
 }
 
