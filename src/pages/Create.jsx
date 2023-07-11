39aaf0eacd992ab9ec8ec926ff7eef625b0b11f5
Jay Dave <davejay3002@gmail.com>
2023-07-11 16:13:05 +0530

    Deploy contract added

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Section9 from '../containers/Section9'
+import Navbar from '../components/Navbars/MainNavbar'
 const Create = () => {
   return (
     <div>
+        <Navbar />
         <Section9 />
     </div>
   )
