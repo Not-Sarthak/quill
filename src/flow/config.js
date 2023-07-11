65749461bead0542383eef81742fb71c9ea15cb1
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-07-11 16:30:15 +0530

    add flowtoken & fungible token addressin config

--- src/flow/config.js
+++ src/flow/config.js
@@ -1,7 +1,9 @@
 import { config } from '@onflow/fcl';
 
 config()
-	.put('accessNode.api', 'https://rest-testnet.onflow.org') // This connects us to Flow TestNet
-	.put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn/') // Allows us to connect to Blocto & Lilico Wallet
-	.put('app.detail.title', 'Quill') // Will be the title when user clicks on wallet
-	.put('app.detail.icon', 'https://imgur.com/GsA9Y0W.png') // Will be the icon when user clicks on wallet
+    .put('accessNode.api', 'https://rest-testnet.onflow.org') // This connects us to Flow TestNet
+    .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn/') // Allows us to connect to Blocto & Lilico Wallet
+    .put('app.detail.title', 'Quill') // Will be the title when user clicks on wallet
+    .put('app.detail.icon', 'https://imgur.com/GsA9Y0W.png') // Will be the icon when user clicks on wallet
+    .put("0xFungibleToken", "0x9a0766d93b6608b7") // Fungible Token Contract Address
+    .put("0xFlowToken", "0x7e60df042a9c0868") // Flow Token Contract Address
