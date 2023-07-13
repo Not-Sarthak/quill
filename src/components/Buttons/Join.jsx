3c470fcb9e832beccc4c11b96c547e41e8d3634d
Jay Dave <davejay3002@gmail.com>
2023-07-13 13:06:37 +0530

    Completed deployment and set Owner details Integration
    Tirth Bhagwat <tirth.bhagwat@proton.me>

--- src/components/Buttons/Join.jsx
+++ src/components/Buttons/Join.jsx
@@ -16,9 +16,9 @@ const Connect = () => {
     <Button>
         <div className="Button" style={{width: 130.55, height: 40, paddingTop: 9, paddingBottom: 9, paddingLeft: 13, paddingRight: 12.55, background: '#1d1f1e', borderRadius: 8, borderLeft: '0.50px rgba(168, 240, 230, 0.60) solid', borderTop: '0.50px rgba(168, 240, 230, 0.60) solid', borderRight: '0.50px rgba(168, 240, 230, 0.60) solid', borderBottom: '0.50px rgba(168, 240, 230, 0.60) solid', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
             <Typography variant="body3">
-              <div style={{color: "#a8f0cb", display: "flex", justifyContent: "center", alignContent: "center",}}>
+              <p style={{color: "#a8f0cb", display: "flex", justifyContent: "center", alignContent: "center",}}>
                 Join
-              </div>
+              </p>
             </Typography>
         </div>
     </Button>
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Section9 from '../containers/Section9'
-import Navbar from '../components/Navbars/MainNavbar'
+import React from "react";
+import Section9 from "../containers/Section9";
+import Navbar from "../components/Navbars/MainNavbar";
+import "../App.css";
 const Create = () => {
   return (
     <div>
-        <Navbar />
-        <Section9 />
+      <Navbar />
+      <Section9 />
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
