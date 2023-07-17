46a331d90a9badc389e87ae7f53d45bec37fadce
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-07-17 17:08:01 +0530

    update cors

--- addresses_endpoint/app.py
+++ addresses_endpoint/app.py
@@ -5,9 +5,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, origins=[
-    'http://localhost:5173', 'https://btirth.me', 'http://btirth.me'
-])
+CORS(app, origins='https://btirth.me')
 
 # get env variable
 API_KEY = os.environ.get('API_KEY')
