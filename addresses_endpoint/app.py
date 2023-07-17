ec64386d9768d767b8c20a81a3627bb970de11c0
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-07-17 16:58:59 +0530

    add cors

--- addresses_endpoint/app.py
+++ addresses_endpoint/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app, origins=[
-    'http://localhost:5173', 'https://btirth.me'
+    'http://localhost:5173', 'https://btirth.me', 'http://btirth.me'
 ])
 
 # get env variable
